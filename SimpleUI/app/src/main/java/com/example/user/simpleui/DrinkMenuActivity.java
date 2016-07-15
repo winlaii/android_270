e81c9aee25015ffefa49efab6649c7e0daf654c0
winlaii <winniel16111086@tas.tw>
2016-07-15 11:42:07 +0800

    add DrinkOrderDialog

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -1,5 +1,8 @@
 package com.example.user.simpleui;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +18,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DrinkMenuActivity extends AppCompatActivity {
+public class DrinkMenuActivity extends AppCompatActivity implements DrinkOrderDialog.OnDrinkOrderListener {
 
     TextView totalTextView;
     ListView drinkMenuListView;
@@ -62,13 +65,28 @@ public class DrinkMenuActivity extends AppCompatActivity {
         drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                DrinkAdapter drinkAdapter = (DrinkAdapter)parent.getAdapter();
-                Drink drink = (Drink)drinkAdapter.getItem(position);
-                orders.add(drink);
-                updateTotal();
+                DrinkAdapter drinkAdapter = (DrinkAdapter) parent.getAdapter();
+                Drink drink = (Drink) drinkAdapter.getItem(position);
+                showDrinkOrderDialog(drink);
             }
         });
     }
+
+    public void showDrinkOrderDialog(Drink drink) {
+        FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        DrinkOrderDialog dialog = DrinkOrderDialog.newInstance("", "");
+        Fragment prev = getFragmentManager().findFragmentByTag("DrinkOrderDialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        dialog.show(ft, "DrinkOrderDialog");
+    }
+
     public void updateTotal()
     {
         int total = 0;
@@ -130,5 +148,10 @@ public class DrinkMenuActivity extends AppCompatActivity {
     public void onRestart(){
         super.onRestart();
         Log.d("Debug", "DrinkMenuActivity onRestart");
+    }
+
+    @Override
+    public void onDrinkOrderFinished() {
+
     }
 }
